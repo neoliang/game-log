b6b0fc39a2b9c9d1e4f6b98568bd5a882b1d41b0
neo <liangneo@qq.com>
2016-01-16 15:47:18 +0800

    update

--- server/public/app/controller/log_table_view_controller.js
+++ server/public/app/controller/log_table_view_controller.js
@@ -27,8 +27,7 @@ var getContent = function  (content,display,hashData) {
 					nv = hashData[content[key]];
 				}
 				if(nv){
-					nv = nv.toString();
-					body += '<tr ><td>' + name +  '</td><td>' + nv.replace("\n","<br/>") +  '</td></tr>';
+					body += '<tr ><td>' + name +  '</td><td>' + nv +  '</td></tr>';
 				}
 			};
 		});
